302c1dfa6cee1c12ceccccbead221d37bde522fd
Peter Petrinec <petrinecp@gmail.com>
2015-10-04 22:02:52 +0200

    Init commit with Task 1 completed.

---
+++ app/src/main/java/cz/muni/fi/pv256/movio/uco396110/App.java
@@ -0,0 +1,25 @@
+package cz.muni.fi.pv256.movio.uco396110;
+
+import android.app.Application;
+import android.os.Build;
+import android.os.StrictMode;
+
+public class App extends Application {
+    public void initStrictMode() {
+        StrictMode.ThreadPolicy.Builder tpb = new StrictMode.ThreadPolicy.Builder()
+                .detectAll()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            tpb.penaltyFlashScreen();
+        }
+        StrictMode.setThreadPolicy(tpb.build());
+
+        StrictMode.VmPolicy.Builder vmpb = new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .penaltyLog();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            vmpb.detectLeakedClosableObjects();
+        }
+        StrictMode.setVmPolicy(vmpb.build());
+    }
+}
